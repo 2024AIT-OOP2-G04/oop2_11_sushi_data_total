110e13324c76bd8669e2fd63037ea2f12891cec5
hinata0601 <hinata0601kaji@icloud.com>
2024-12-13 09:22:02 +0900

    add:テーブル別利用回数の追加

--- routes/api/number_of_usage_by_table.py
+++ routes/api/number_of_usage_by_table.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from flask import Blueprint, render_template, request, redirect, url_for
 from models import Customer
 import json
@@ -9,4 +10,11 @@ def number_of_usage_by_table():
     customers = Customer.select()
     print("data", customers)
     data = [{"id": customer.id, "table": customer.table} for customer in customers]
-    return json.dumps(data)
+    table_data = [entry["table"] for entry in data]
+    table_usage_count = Counter(table_data)
+    total_usage_count = sum(table_usage_count.values())
+    response_data = {
+        "table_usage": sorted([{"table": table, "usage_count": count} for table, count in table_usage_count.items()],key=lambda x: x["table"]),
+        "total_usage_count": total_usage_count
+    }
+    return json.dumps(response_data)
