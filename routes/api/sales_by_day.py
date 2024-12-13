c077448226e68a65d7821b3d8d2f99640ed2afd7
Bunzin04 <kawa.fumi.2004@icloud.com>
2024-12-13 12:52:54 +0900

    add 曜日別売上表示

--- routes/api/sales_by_day.py
+++ routes/api/sales_by_day.py
@@ -7,7 +7,20 @@ from .base import api_bp
 @api_bp.route("/sales_by_day", methods=["GET"])
 def sales_by_day():
     orders = Order.select()
-    print("data", orders)
-    data = [{"id": order.id, "date": order.order_date} for order in orders]
-    print("data", data)
-    return json.dumps(data)
+
+    amount_data = [0,0,0,0,0,0,0]
+    for element in orders:
+        _weekday = element.order_date.weekday()
+        amount_data[_weekday] += int(element.good.price)
+
+    data = {
+        "月曜日": amount_data[0],
+        "火曜日": amount_data[1],
+        "水曜日": amount_data[2],
+        "木曜日": amount_data[3],
+        "金曜日": amount_data[4],
+        "土曜日": amount_data[5],
+        "日曜日": amount_data[6],
+    }
+
+    return json.dumps(data , ensure_ascii=False)
