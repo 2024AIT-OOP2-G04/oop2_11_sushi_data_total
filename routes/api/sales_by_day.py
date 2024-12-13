8cc30d02caa3d4d01e8033a14a5970341e196ea4
Bunzin04 <kawa.fumi.2004@icloud.com>
2024-12-13 09:12:47 +0900

    とりあえずベース作ったやつです

--- routes/api/sales_by_day.py
+++ routes/api/sales_by_day.py
@@ -1,5 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from models import Customer
+from models import Order
 import json
 
 from .base import api_bp
+
+@api_bp.route("/sales_by_day", methods=["GET"])
+def sales_by_day():
+    orders = Order.select()
+    print("data", orders)
+    data = [{"id": order.id, "date": order.order_date} for order in orders]
+    print("data", data)
+    return json.dumps(data)
