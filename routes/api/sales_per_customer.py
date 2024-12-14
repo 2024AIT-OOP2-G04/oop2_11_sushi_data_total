13dd0b2b50ed5771b21b960bb6b8cf467abfa231
Ryu <136865700+k23023-university-student@users.noreply.github.com>
2024-12-14 20:56:29 +0900

    Update: JSONの配列形式で出力するように変更

--- routes/api/sales_per_customer.py
+++ routes/api/sales_per_customer.py
@@ -15,16 +15,18 @@ def get_sales_per_customer():
     # 配列に整理
     customers_array = [{"id": customer.id, "table": customer.table} for customer in customers]
 
-    data = {}
+    data = []
     for customer in customers_array:##客の数だけforループ
+
         customer_id = customer["id"]
         # その客に合致する注文のリストを表示
         orders = Order.select().where(Order.customer == customer_id)
         total_sales = 0
+
         for order in orders:## 商品の値段をループで取得
             good = Goods.get_or_none(Goods.id == order.good)
             if good:
                 total_sales += int(good.price)
-        data[customer_id] = total_sales
+        data.append({"id": customer_id, "total_sales": total_sales})
 
     return json.dumps(data)
