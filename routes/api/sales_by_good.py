5f7fd2c62737eb69f3b9c404df4d8090859450b0
k23096 <k23096kk@aitech.ac.jp>
2024-12-14 22:39:15 +0900

    k23096

--- routes/api/sales_by_good.py
+++ routes/api/sales_by_good.py
@@ -9,5 +9,14 @@ from .base import api_bp
 def number_of_usage_by_table():
     orders = Order.select()
     print("data", orders)
+    l=[]
+    sumprice=[]
+    for order in orders:
+        if(l.count(order.good.id)<=0):
+            l.append(order.good.id)
+            sumprice.append(float(order.good.price))
+        
+        sumprice[l.index(order.good.id)]=sumprice[l.index(order.good.id)]+float(order.good.price)
+
     data = [{"id": order.id, "goodsid": order.good.id,"goodsprice":float(order.good.price)} for order in orders]
     return json.dumps(data)
