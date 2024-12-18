fa330c2f550dfc9e70ebd98de87d95cf5f82f254
k23096 <k23096kk@aitech.ac.jp>
2024-12-18 11:25:46 +0900

    いろいろ

--- routes/api/number_of_usage_by_table.py
+++ routes/api/number_of_usage_by_table.py
@@ -4,9 +4,9 @@ import json
 
 from .base import api_bp
 
-@api_bp.route("/number_of_usage_by_table", methods=["GET"])
-def number_of_usage_by_table():
-    customers = Customer.select()
-    print("data", customers)
-    data = [{"id": customer.id, "table": customer.table} for customer in customers]
-    return json.dumps(data)
+#@api_bp.route("/number_of_usage_by_table", methods=["GET"])
+#def number_of_usage_by_table():
+#    customers = Customer.select()
+#    print("data", customers)
+#    data = [{"id": customer.id, "table": customer.table} for customer in customers]
+#    return json.dumps(data)
--- static/sales_by_good.js
+++ static/sales_by_good.js
@@ -1,8 +1,38 @@
-let current
-fetch("/api/sales_by_good")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log("orderdata", data);
-    current=data;
-  });
-let objData = JSON.parse(current);
+let data2
+let res
+  function fetchData() {
+    data2 =  fetch("api/sales_by_good");
+    res =  data2.json();
+    console.log(res);
+  }
+  fetchData();
+  console.log("orderdata", res);
+let ctx = document.getElementById('ex_chart');
+
+let data = {
+    labels: [],
+    datasets: [{
+        label: '合計金額',
+        data: [],
+        backgroundColor: 'rgba(255, 100, 100, 1)'
+    }]
+};
+for(let i=0;i<5;i++){
+  data.labels.push(objData.id[i])
+  data.data.push(objData.sumprice[i])
+}
+let options = {
+  indexAxis: 'y',
+    scales: {
+        x:{
+            min: 300
+            //beginAtZero: true
+        }
+    }
+};
+
+let ex_chart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: options
+});
